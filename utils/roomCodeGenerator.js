d15ca01da8f9219d4e03e7f06d423e4cb2800b9e
sumit <singhsk242004@gmail.com>
2026-02-03 01:23:42 +0530

    code_gen

---
+++ utils/roomCodeGenerator.js
@@ -0,0 +1,10 @@
+const generateRoomCode = () => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let result = '';
+    for (let i = 0; i < 6; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+};
+
+module.exports = generateRoomCode;
