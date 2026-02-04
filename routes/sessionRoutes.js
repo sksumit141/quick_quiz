e2f5957cc4fd1190c173289eb9dbe07ffdd684bd
sumit <singhsk242004@gmail.com>
2026-02-05 01:18:48 +0530

    sessionroute

---
+++ routes/sessionRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    createSession,
+    getSessionByRoomCode,
+    endSession,
+    updateCurrentQuestion
+} = require('../controllers/sessionController');
+
+router.route('/')
+    .post(createSession);
+
+router.route('/room/:roomCode')
+    .get(getSessionByRoomCode);
+
+router.route('/:id/end')
+    .put(endSession);
+
+router.route('/:id/question')
+    .put(updateCurrentQuestion);
+
+module.exports = router;
